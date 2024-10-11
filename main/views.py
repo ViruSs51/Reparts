566e463852480084a9a426e3301fd4a962006843
Katsuryoku <vitalieberzoi555@gmail.com>
2024-10-11 21:54:52 +0300

    Initial MVP structure

--- main/views.py
+++ main/views.py
@@ -1,9 +1,7 @@
 from django.shortcuts import render
 
-
+# Create your views here.
 def home(request):
-    data = {
-        'page': 'home'
-    }
+    data = {}
 
-    return render(request, "main-pages/home.html", data)
+    return render(request, 'main/home.html', data)
