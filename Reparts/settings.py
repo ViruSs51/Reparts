f46e176e43e46acea81448f499d3b5b07b26e816
Katsuryoku <vitalieberzoi555@gmail.com>
2024-10-19 00:14:44 +0300

    Update settings

--- Reparts/settings.py
+++ Reparts/settings.py
@@ -78,15 +78,22 @@ TEMPLATES = [
 
 CACHES = {
     'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+        'LOCATION': 'unique-snowflake',
     }
 }
 
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
+#CACHES = {
+#    'default': {
+#        'BACKEND': 'django_redis.cache.RedisCache',
+#        'LOCATION': 'redis://127.0.0.1:6379/1',
+#        'OPTIONS': {
+#            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+#        }
+#    }
+#}
+#
+#CELERY_BROKER_URL = 'redis://127.0.0.1:6379/0'
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.cache'
 SESSION_CACHE_ALIAS = 'default'
---
+++ authentication/forms.py
@@ -0,0 +1,34 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(
+        max_length=30, 
+        required=False, 
+        widget=forms.TextInput(attrs={'placeholder': 'Имя'}),
+        help_text='Optional.'
+    )
+    last_name = forms.CharField(
+        max_length=30, 
+        required=False, 
+        widget=forms.TextInput(attrs={'placeholder': 'Фамилия'}),
+        help_text='Optional.'
+    )
+    email = forms.EmailField(
+        max_length=254, 
+        widget=forms.EmailInput(attrs={'placeholder': 'Электронная почта'}),
+        help_text='Required. Enter a valid email address.'
+    )
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}),
+        label='Password'
+    )
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={'placeholder': 'Повторите пароль'}),
+        label='Password confirmation'
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
